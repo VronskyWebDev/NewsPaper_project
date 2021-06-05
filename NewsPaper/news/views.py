a740ac9de6f7f3e067a69e33e215fd7a105804bf
Alexey <alexey.vronsky.studio@gmail.com>
2021-06-05 13:25:33 +0300

    Created news page html and added ListView, DetailView modules

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from django.views.generic import DetailView
+
+
+class PostList(ListView):
+    model = Post  # указываем модель, объекты которой мы будем выводить
+    template_name = 'news.html'
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'news.html'
+    context_object_name = 'news'
